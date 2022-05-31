6d1a89ba2f16ce92cf9aa45e425c7f9ea4a45096
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-31 10:24:51 +0600

    first commit

---
+++ convertion.js
@@ -0,0 +1,58 @@
+// string number
+var firstName = "sakib";
+var lastNAme = "rakib";
+var fullName = firstName + ' ' + lastNAme;
+console.log(fullName);
+var firstName1 = "ariana";
+var lastNAme1 = "golla";
+console.log(firstName1 + ' ' + lastNAme1);
+
+
+
+// string number
+var onionPrice = '42';
+var eggPrice = '41';
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+
+
+
+// integer number
+var onionPrice = 42;
+var eggPrice = 41;
+
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+
+
+
+// float number 
+var price = 99.93;
+
+
+
+
+// string number ke parseIn diye integer number e rupantor kora jay
+var onionPrice = '42';
+var eggPrice = '41';
+var onionPriceNumber = parseInt(onionPrice);
+console.log(onionPriceNumber);
+
+
+
+
+// string number ke parseIn diye float number e rupantor kora jay and sumation kora jay.....
+var onionPrice = '42.93';
+var eggPrice = '41.33';
+var onionPriceNumber = parseFloat(onionPrice);
+var eggPriceNumber = parseFloat(onionPrice);
+console.log(onionPriceNumber + eggPriceNumber);
+
+
+
+
+
+
+
